560da32879ac475d1b18224163779f86fffda4c9
Jan Ploski <jpl@plosquare.com>
2023-08-08 21:37:03 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RotaryEmbedding
-Comparison of RoPE and xPos positional embeddings used in LLMs
+
+Side-by-side comparison of the RoPE and xPos positional embedding algorithms
+used in LLMs.
+
+This repository contains a self-contained Python implementation of each
+positional embeddings.  The xPos embedding was invented as an enhancement
+to RoPE, addressing problems with extrapolation and undesirable cyclical
+oscillations of attention scores in the latter with increasing token distance.
+
+The implementation of RoPE (rope.py) is borrowed from [Falcon LLM](https://huggingface.co/tiiuae/falcon-7b/blob/main/modelling_RW.py#L39)
+(which in turn borrowed it from GPT-NeoX). The implementation of xPos (xpos.py) is created by changing rope.py based on the code included in [syncdoth/RetNet](https://github.com/syncdoth/RetNet)
+(which in turn borrowed it from [torchscale](https://github.com/microsoft/torchscale/blob/main/torchscale/component/xpos_relative_position.py)).
+
+Links to the original papers:
+ * [RoPE](https://arxiv.org/pdf/2104.09864.pdf)
+ * [xPos](https://arxiv.org/pdf/2212.10554.pdf)
---
+++ xpos.py
@@ -0,0 +1,72 @@
+# https://arxiv.org/pdf/2212.10554.pdf
+# https://github.com/syncdoth/RetNet/blob/main/retnet/xpos_relative_position.py
+from einops import rearrange
+
+# [1,2]    [1,1,2,2]
+# [3,4] -> [3,3,4,4]
+# [5,6]    [5,5,6,6]
+def duplicate_interleave(m):
+    return m.view(-1, 1).repeat(1, 2).view(m.shape[0], -1)
+
+# 0,1,2,3,4,5,6,7 -> -1,0,-3,2,-5,4,-7,6
+def rotate_every_two(x):
+    x1 = x[:, :, ::2]
+    x2 = x[:, :, 1::2]
+    x = torch.stack((-x2, x1), dim=-1)
+    return x.flatten(-2)  # in einsum notation: rearrange(x, '... d j -> ... (d j)')\
+
+
+class XPosEmbedding(torch.nn.Module):
+    """Implementation of xPos based on RotaryEmbedding from GPT-NeoX.
+    This implementation is designed to operate on queries and keys that are compatible with
+    [batch_size, n_heads_per_partition, seq_len, head_dim] (e.g. MinGPTAttention format).
+    """
+
+    def __init__(
+        self,
+        head_dim: int,
+        base=10000,
+        scale_base=512
+    ):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.head_dim = head_dim
+        self.seq_len_cached = None
+        self.batch_size_cached = None
+        self.cos_cached: torch.Tensor | None = None
+        self.sin_cached: torch.Tensor | None = None
+        self.scale_base = scale_base
+        self.register_buffer("scale",
+                             (torch.arange(0, head_dim, 2) + 0.4 * head_dim) / (1.4 * head_dim))
+
+    def cos_sin(
+        self,
+        seq_len: int,
+        device="cuda",
+        dtype=torch.bfloat16,
+    ) -> torch.Tensor:
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            t = torch.arange(seq_len, device=device).type_as(self.inv_freq)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            emb = duplicate_interleave(freqs).to(device)
+
+            if dtype in [torch.float16, torch.bfloat16]:
+                emb = emb.float()
+
+            self.cos_cached = emb.cos()[None, :, :]
+            self.sin_cached = emb.sin()[None, :, :]
+
+            self.cos_cached = self.cos_cached.type(dtype)
+            self.sin_cached = self.sin_cached.type(dtype)
+
+        return self.cos_cached, self.sin_cached
+
+    def forward(self, q, k):
+        batch, seq_len, head_dim = q.shape
+        cos, sin = self.cos_sin(seq_len, q.device, q.dtype)
+        scale = self.scale**torch.arange(seq_len).to(self.scale).div(self.scale_base)[:, None]
+        scale = duplicate_interleave(scale)
+        return (q * cos * scale) + (rotate_every_two(q) * sin * scale),
+               (k * cos * (1 / scale)) + (rotate_every_two(k) * sin * (1 / scale))
